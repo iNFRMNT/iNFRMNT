0d0e82369964af29419ba81be453374711f03d18
Tyler Kadow <tylerkadow@aol.com>
2017-12-09 12:44:33 -0500

    a few additions

--- assets/js.js
+++ assets/js.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
-  $(document).ready(function(){
+  $('[name="news"]').click(function(){
     $.ajax({
-      url: "https://newsapi.org/v2/everything?sources=breitbart-news&apiKey=35477fbe6cda44b0853c42734970825a",
+      url: "https://newsapi.org/v2/everything?sources=" + this.id + "&apiKey=35477fbe6cda44b0853c42734970825a",
       type: "GET",
       dataType: 'json',
     }).done(function(data){
       console.log(data);
+
+      $("#news-stories").empty();
 
       for (var i = 0; i < 10; i++) {
         $('#news-stories').append(
@@ -15,22 +17,12 @@ $(document).ready(function(){
     });
   });
 
-  $(document).ready(function(){
-    $.ajax({
-      url: "https://newsapi.org/v2/everything?sources=abc-news&apiKey=35477fbe6cda44b0853c42734970825a",
-      type: "GET",
-      dataType: 'json',
-    }).done(function(data){
-      console.log(data);
-
-      for (var i = 0; i < 10; i++) {
-        $('#news').append(
-        "<a target='_blank' href='" + data.articles[i].url + "'><b>" + data.articles[i].title + "</b></a><div class='space'></div>");
-      }
-    });
-  });
 
   $('[name="state"]').click(function() {
+
+    $(".state").empty();
+    $(".state").append(this.id);
+
     $.ajax({
       url: "https://api.propublica.org/congress/v1/members/senate/" + this.id + "/current.json",
       type: "GET",
@@ -44,7 +36,7 @@ $(document).ready(function(){
 
       for (var i = 0; i < data.results.length; i++) {
         $('#senators').append(
-        "<p>" + data.results[i].name + ": " + data.results[i].party + " " + "<a target='_blank' href='http://twitter.com/" + data.results[i].twitter_id + "'><img src='assets/twitter.png' class='social_media'></a><a target='_blank' href='http://youtube.com/" + data.results[i].youtube_id + "'><img src='assets/youtube.png' class='social_media'></a></p>");
+        "<p>" + data.results[i].name + ": " + data.results[i].party + " " + "<a target='_blank' href='http://twitter.com/" + data.results[i].twitter_id + "'><img src='assets/twitter.png' class='social_media'></a> <a target='_blank' href='http://youtube.com/" + data.results[i].youtube_id + "'><img src='assets/youtube.png' class='social_media'></a> <a target='_blank' href='http://facebook.com/" + data.results[i].facebook_account + "'><img src='assets/facebook.png' class='social_media'></a></p>");
       }
     });
   });
@@ -63,7 +55,7 @@ $(document).ready(function(){
 
       for (var i = 0; i < data.results.length; i++) {
         $('#house').append(
-        "<p>" + data.results[i].name + ": " + data.results[i].party + " " + "<a target='_blank' href='http://twitter.com/" + data.results[i].twitter_id + "'><img src='assets/twitter.png' class='social_media'></a><a target='_blank' href='http://youtube.com/" + data.results[i].youtube_id + "'><img src='assets/youtube.png' class='social_media'></a></p>");
+        "<p>" + data.results[i].name + ": " + data.results[i].party + " " + "<a target='_blank' href='http://twitter.com/" + data.results[i].twitter_id + "'><img src='assets/twitter.png' class='social_media'></a><a target='_blank' href='http://youtube.com/" + data.results[i].youtube_id + "'><img src='assets/youtube.png' class='social_media'></a> <a target='_blank' href='http://facebook.com/" + data.results[i].facebook_account + "'><img src='assets/facebook.png' class='social_media'></a></p>");
       }
     });
   });
