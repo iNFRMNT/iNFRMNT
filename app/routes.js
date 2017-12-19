cc92789392609bd1b97f539325e4644eadf2a8a6
Mike S <mjstephenson@me.com>
2017-12-18 20:18:05 -0500

    Changed routes so that the page redirects to the login page after a sucessful account creation.

--- app/routes.js
+++ app/routes.js
@@ -41,7 +41,7 @@ module.exports = function(app, passport) {
     // app.post('/signup', do all our passport stuff here);
 
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/login', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
